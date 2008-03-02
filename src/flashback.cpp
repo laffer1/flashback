b5be2a7b1a8cd73056b9a6e453b4e0d55771094b
wyverex <wyverex>
2008-03-02 18:29:32 +0000

    Add seperation for Windows/nix to MAKEFILE and flashback.cpp.

--- src/flashback.cpp
+++ src/flashback.cpp
@@ -1,12 +1,34 @@
-
 #include <iostream>
 using namespace std;
 
+#ifdef Win32
+	#include <windows.h>
 
+int WINAPI WinMain(HINSTANCE  hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nShow)
+{
+#ifdef Debug
+	cout << "Running in Debug Mode!" << endl;
+	cout << "Windows Version" << endl;
+#endif
+	//Call Flashback Start
+	return 0;
+}
 
-
+#else
 
 int main(int argc, char** args)
 {
+#ifdef Debug
+	cout << "Running in Debug Mode!" << endl;
+#endif
+
+	//Call Flashback Start
 	return 0;
-}
+}	
+#endif
+
+
+
+
+
+
