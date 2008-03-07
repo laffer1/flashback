84464e83395616b0d5a500728a43c7c520e53197
wyverex <wyverex>
2008-03-07 22:50:00 +0000

    Added Critical Section Object..  Works good with windows, needs linux, bsd testing...

---
+++ src/fbCriticalSection.cpp
@@ -0,0 +1,92 @@
+/**
+*	fbCriticalSection
+*	Creates a CS lock to prevent threads 
+*	from reading and writing at the same time
+*	@author Byron Heads
+*	@date March 07, 2008
+*/
+
+#include "fbCriticalSection.h"
+
+/**
+*	fbCriticalSection
+*	Default Critical Section constructor
+*	@note Initilize all member vars
+*/
+fbCriticalSection::fbCriticalSection():_locked(false)
+{
+#ifdef Win32
+	InitializeCriticalSection(&hCriticalSection);	/// < Create CS object
+#else
+	 pthread_mutex_init(&hMutex, NULL);
+#endif
+}
+
+/**
+*	~fbCriticalSection
+*	Destructor, Delete CS object
+*/
+fbCriticalSection::~fbCriticalSection()
+{
+#ifdef Win32
+	DeleteCriticalSection(&hCriticalSection);
+#else
+	pthread_mutex_destroy(&hMutex);
+#endif
+}
+
+/**
+*	lock
+*	Locks the CS, or blocks thread till CS is unlocked
+*/
+void fbCriticalSection::lock()
+{
+#ifdef Win32
+	EnterCriticalSection(&hCriticalSection);
+#else
+	 pthread_mutex_lock(&hMutex);
+#endif
+	_locked = true;	/// < remember CS is locked
+}
+
+/**
+*	unlock
+*	unlocks a locked CS and runs first block thread
+*/
+void fbCriticalSection::unlock()
+{
+	_locked = false;	/// < mark unlocked
+#ifdef Win32
+	LeaveCriticalSection(&hCriticalSection);
+#else
+	 pthread_mutex_unlock(&hMutex);
+#endif	
+}
+
+/**
+*	isLocked
+*	Test if thread is currently locked
+*	@return True is CS is locked
+*/
+bool fbCriticalSection::isLocked()
+{
+	return _locked;
+}
+
+/**
+*	tryLock
+*	Tries to lock CS, but doesn't block if locked
+*	@return True if was able to get lock
+*/
+bool fbCriticalSection::tryLock()
+{
+#ifdef Win32
+	if(!TryEnterCriticalSection(&hCriticalSection))
+		return false;
+#else
+	if(!pthread_mutex_trylock(&hMutex))
+		return false;
+#endif
+	_locked = true;
+	return true;
+}
---
+++ src/fbCriticalSection.h
@@ -0,0 +1,35 @@
+#ifndef fbCRITICALSECTION_H
+#define fbCRITICALSECTION_H
+
+#include "global.h"
+
+/**
+*	fbCriticalSection
+*	Critical Section Object
+*	@author Byron Heads
+*	@date March 07, 2008
+*	@note 3/5/08 - Created fbCriticalSection
+*/
+class fbCriticalSection
+{
+public:
+	fbCriticalSection();	/// < Default constructor
+	~fbCriticalSection();	/// < Destructor
+
+	void lock();			/// < Lock the CS or block untill its unlocked
+	void unlock();			/// < unlock the CS
+	
+	bool isLocked();		/// < Test if the CS is locked
+	bool tryLock();			/// < Lock the CS but dont block if failed
+
+private:
+	bool _locked;			/// < True if Cs is locked
+
+#ifdef Win32
+	CRITICAL_SECTION hCriticalSection;
+#else
+	pthread_mutex_t	 hMutex;
+#endif
+};
+
+#endif
