b35385718bf82ca1d7c0838f1e34a3ca2c0b2e85
laffer1 <laffer1>
2008-03-08 05:12:28 +0000

    Add some UNIX friendly safe programming practices.
    fork from parent so we have a daemon
    Close all file descriptors for stdin/stdout/stderr so we are totally detached from console.
    become a session leader i.e. parent process is init
    clear umask just in case.

--- src/flashback.cpp
+++ src/flashback.cpp
@@ -12,6 +12,30 @@ int main(int argc, char** args)
 {
 #ifdef Debug
 	cout << "Running in Debug Mode!" << endl;
+#else
+	switch( fork() )
+	{
+		// DIDNT WORK
+		case -1:
+			perror("fork");
+			return -1;
+			break;
+
+		// parent process returns value > 0 upon success (child id)
+		default:
+			return 0;
+
+		case 0:
+			// close unneeded file descriptors
+			fclose(stdout);
+			fclose(stdin);
+			fclose(stderr);
+	
+			// TODO: make this Windows friendly	
+			setsid(); // become session leader
+			chdir("/"); // change wrkdir to root
+			umask(0); // clear file mode creation mask
+			break;	
 #endif
 
 	//Call Flashback Start
