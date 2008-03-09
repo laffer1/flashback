9e8581044f3aa5f73589090e120e83963bd85fab
laffer1 <laffer1>
2008-03-09 18:02:12 +0000

    Apparently windows can't even fork things.  How pathetic.
    Byron (or someone not doing anything) will need to find out what the required steps are to make a service on win32 and implement that instead of the fork and other methods here.

--- src/flashback.cpp
+++ src/flashback.cpp
@@ -1,4 +1,4 @@
-/* $Id: flashback.cpp,v 1.9 2008/03/09 16:43:39 wyverex Exp $ */
+/* $Id: flashback.cpp,v 1.10 2008/03/09 18:02:12 laffer1 Exp $ */
 
 #include "global.h"  /// < Holds Common Global Header Files
 
@@ -14,8 +14,15 @@ int main(int argc, char** args)
 #ifdef Debug
 	cout << "Running in Debug Mode!" << endl;
 #else
-#ifndef WIN32	
-			// TODO: make this Windows friendly	
+#ifndef WIN32
+	/* In UNIX programming, a daemon MUST let go of the terminal it is 
+           running on.  Otherwise, when you log out, the process dies.
+           The solution is to fork a copy of yourself and then disconnect
+           ties to the original terminal like stdin, stdout, stderr
+           The windows alternative would be whatever one must do to
+           get a "service" in a microsoft product.
+        */
+	// TODO: make this Windows friendly	
 	switch( fork() )
 	{
 		// DIDNT WORK
@@ -34,7 +41,7 @@ int main(int argc, char** args)
 			fclose(stdin);
 			fclose(stderr);
 
-
+			/* safety features */
 			setsid(); // become session leader
 			chdir("/"); // change wrkdir to root
 			umask(0); // clear file mode creation mask
