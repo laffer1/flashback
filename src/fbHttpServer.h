c332cbb81f37f779df87e6ab2d35ad3539e80f13
laffer1 <laffer1>
2008-03-16 00:47:10 +0000

    The start of the HTTP server code.

---
+++ src/fbHttpServer.h
@@ -0,0 +1,52 @@
+/* $Id: fbHttpServer.h,v 1.1 2008/03/16 00:47:10 laffer1 Exp $ */
+/*-
+ * Copyright (C) 2008 Lucas Holt. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef FBHTTPSERVER_H
+#define FBHTTPSERVER_H
+
+#include "fbClient.h"
+
+/**
+ * fbClient
+ * Client connted via socket for use with HTTP.
+* Stores HTTP Request and state of this www user.
+ * @author Lucas Holt
+ * @date March 15, 2008
+ */
+class fbHttpServer
+{
+public:
+    fbHttpServer( fbErrorLogger &err );   /// default constructor
+    ~fbHttpServer();  /// default destructor
+
+    void start();  /// Start HTTP server
+    void stop();  /// Stop HTTP server
+
+protected:
+    bool keeprunning;
+};
+
+#endif
