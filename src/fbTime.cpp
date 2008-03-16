16324fc9116050014fd696b84bfa68ffbbee83a5
wyverex <wyverex>
2008-03-16 01:56:11 +0000

    Create a time object holds secounds from midnight simple enough...

---
+++ src/fbTime.cpp
@@ -0,0 +1,103 @@
+/* $Id: fbTime.cpp,v 1.1 2008/03/16 01:56:11 wyverex Exp $ */
+
+
+/**
+ *  fbTime
+ *   Time manager
+ *  counts the secounds from midnight
+ *  @author Byron Heads
+ *  @date March 15, 2008
+ */
+
+#include "fbTime.h"
+
+/*
+ *  fbTime
+ *  Default constructor, initilizes to current local time
+ */
+fbTime::fbTime():ticks(0), hour(0), min(0), sec(0)
+{
+	setTicksLocal();
+}
+
+/*
+ *  fbTime
+ *  Constructor, sets time to given values
+ *  @param h Hours
+ *  @param m Mins
+ *  @param s Sounds
+ */
+fbTime::fbTime(int h, int m, int s):ticks(0), hour(0), min(0), sec(0)
+{
+	setTicks(h,m,s);
+}
+
+/*
+ *  fbTime
+ *  Constructor, sets time to give tick values, loaded from file?
+ *  @param _ticks new tick value
+ */
+fbTime::fbTime(long _ticks):ticks(_ticks), hour(0), min(0), sec(0)
+{
+	update();
+}
+
+/*
+ *  fbTime
+ *   constructor, seeds time with tm struct
+ *  @param time, tm struct ref
+ */
+fbTime::fbTime(tm& time):ticks(0), hour(0), min(0), sec(0)
+{
+	setTicks(time.tm_hour, time.tm_min, time.tm_sec);
+}
+
+/*
+ *  fbTime
+ *   constructor, copies an existing time structures tick value
+ *  @param time, fbTime to copy
+ */
+fbTime::fbTime(fbTime& time):ticks(time.ticks), hour(0), min(0), sec(0)
+{
+	update();
+}
+	
+/*
+ *  setTicks
+ *  Sets the ticks to given time
+ *  @param h Hours
+ *  @param m Mins
+ *  @param s Sounds
+ */
+void fbTime::setTicks(int h, int m, int s)
+{
+	ticks = (h * 3600) + (m * 60) + s;
+	update();
+}
+
+/*
+ *  setTicksLocal
+ *  Sets the ticks to the current local time
+ */
+void fbTime::setTicksLocal()
+{
+	time_t raw;
+	time(&raw);
+	tm time = *localtime(&raw);
+	setTicks(time.tm_hour, time.tm_min, time.tm_sec);
+}
+
+/*
+ *  update
+ *  recalculates time based on ticks
+ *  call it after anythime ticks is changed!!
+ */
+void fbTime::update()
+{
+	ticks %= 86400;
+
+	hour = ticks / 3600;
+	sec = ticks % 60;
+	min = (ticks - (hour * 3600) - sec) / 60;
+}
+
---
+++ src/fbTime.h
@@ -0,0 +1,75 @@
+/* $Id% */
+
+
+#ifndef fbTIME_H
+#define fbTIME_H
+
+#include "global.h"
+#include <ctime>
+
+/**
+ *  fbTime
+ *   Time manager
+ *  counts the secounds from midnight
+ *  @author Byron Heads
+ *  @date March 15, 2008
+ */
+class fbTime
+{
+public:
+	fbTime();
+	fbTime(int h, int m, int s);
+	fbTime(long _ticks);
+	fbTime(tm& time);
+	fbTime(fbTime& time);
+
+	~fbTime(){};
+
+	//get and set mostly used for saving..
+	long getTicks() const {return ticks;};	
+	void setTicks(long _ticks) {ticks = _ticks;};
+
+	// set the tick value
+	void setTicks(tm& time){setTicks(time.tm_hour, time.tm_min, time.tm_sec);};
+	void setTicks(int h, int m, int s);
+	void setTicksLocal();
+	
+	//get the time
+	int getHour() const {return hour;};
+	int getMin() const {return min;};
+	int getSec() const {return sec;}; 
+
+	//comparision operators
+	bool operator==(fbTime& rhs) {return ticks == rhs.ticks;};
+	bool operator>=(fbTime& rhs) {return ticks >= rhs.ticks;};
+	bool operator<=(fbTime& rhs) {return ticks <= rhs.ticks;};
+	bool operator>(fbTime& rhs) {return ticks > rhs.ticks;};
+	bool operator<(fbTime& rhs) {return ticks < rhs.ticks;};
+
+	//simple add secounds
+	fbTime& operator+(const int s){ticks+= s; update(); return *this;};
+	fbTime& operator+=(const int s){ticks+= s; update(); return *this;};
+	fbTime& operator++(){++ticks; update(); return *this;};
+	fbTime& operator++(const int s){++ticks; update(); return *this;};
+	
+	//remove secs
+	fbTime& operator-(const int s){ticks-= s; update(); return *this;};
+	fbTime& operator-=(const int s){ticks-= s; update(); return *this;};
+	fbTime& operator--(){--ticks; update(); return *this;};
+	fbTime& operator--(const int s){--ticks; update(); return *this;};
+
+	void addSec(const int s = 1){*this += s;};
+	void addMin(const int m = 1){*this += (m*60);};
+	void addHour(const int h = 1){*this += (h*3600);};
+
+private:
+	long ticks;
+	int hour;
+	int min;
+	int sec;
+
+	void update();
+};
+
+#endif
+
